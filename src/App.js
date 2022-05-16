d83336fdc4e74014e6eaa30352038906e03cccce
Stefan Lehmann <bernd49634@web.de>
2022-05-16 06:38:58 +0200

    added alchemy, UD and etherscan API calls and now working on displaying all necessary data in the UI

--- src/App.js
+++ src/App.js
@@ -3,36 +3,61 @@ import EtherscanAPI from "./Components/EtherscanAPI";
 import axios from "axios";
 import { Box, Container } from "@mui/material";
 import React, { useState, useEffect } from "react";
+import { ethers } from "ethers";
 function App() {
   const [inputValue, setInputValue] = useState("");
-
+  const [normalTXHistory, setNormalTXHistory] = useState("");
+  const [internalTXHistory, setInternalTXHistory] = useState("");
+  const [isFetchedTX, setIsFetchedTX] = useState(false);
   function changeInput(e) {
     setInputValue(e.target.value);
   }
+  // MY ADDRESS 0x4ad3710fbbf9d1f2cd56031509114e2355e54468
+  // RANDOM TEST ADDRESS 0x23aD6CdC593C4ebF1f124D4aae2422A5848c29c5
   async function getAndChangeInputIntoAddress() {
-    const regex = /0x/;
-    // if user inputted address
-    if (inputValue && inputValue.length !== 64 && inputValue.test(regex)) {
+    const regex = "0x";
+    // if user inputs address
+
+    if (
+      inputValue &&
+      inputValue.length === 42 &&
+      inputValue.substring(0, 2) === regex
+    ) {
       try {
-        let result = await axios.get(
-          `https://api.etherscan.io/api?module=account&action=txlist&address=${address}&startblock=0&endblock=14775340&page=1&offset=5&sort=asc&apikey=${process.env.REACT_APP_ETHERSCAN_API_KEY}`
+        const provider = new ethers.providers.JsonRpcProvider(
+          `https://eth-mainnet.alchemyapi.io/v2/${process.env.REACT_APP_ALCHEMY_KEY}`
         );
+        const blockNumber = await provider.getBlockNumber();
+
+        // ${inputValue}
+        let result = await axios.get(
+          `https://api.etherscan.io/api?module=account&action=txlist&address=0x23aD6CdC593C4ebF1f124D4aae2422A5848c29c5&startblock=0&endblock=${blockNumber}&page=1&offset=5&sort=asc&apikey=${process.env.REACT_APP_ETHERSCAN_API_KEY}`
+        );
+        setNormalTXHistory(result);
         console.log(result);
         result = await axios.get(
-          `https://api.etherscan.io/api?module=account&action=txlistinternal&address=${address}&startblock=0&endblock=14775340&page=1&offset=10&sort=asc&apikey=${process.env.REACT_APP_ETHERSCAN_API_KEY}`
+          `https://api.etherscan.io/api?module=account&action=txlistinternal&address=0x23aD6CdC593C4ebF1f124D4aae2422A5848c29c5&startblock=0&endblock=${blockNumber}&page=1&offset=10&sort=asc&apikey=${process.env.REACT_APP_ETHERSCAN_API_KEY}`
         );
-        console.log(result);
+        setInternalTXHistory(result);
+        setIsFetchedTX(true);
+        console.log("address inputted");
       } catch {
         console.log("etherscan api call ended up failing");
       }
     }
     // if input was Unstoppable Domain
     // call alchemy's unstoppable API
+    else if (inputValue === "" || inputValue === undefined) {
+      console.log("no input value");
+      return;
+    } else {
+      console.log("domain inputted");
+      alchemyUD();
+    }
   }
-  let address = "0x4Ad3710FBBF9d1f2Cd56031509114E2355E54468";
   async function getAPIData() {
     getAndChangeInputIntoAddress();
-    try {
+    /*   try {
       let result = await axios.get(
         `https://api.etherscan.io/api?module=account&action=txlist&address=${address}&startblock=0&endblock=14775340&page=1&offset=5&sort=asc&apikey=${process.env.REACT_APP_ETHERSCAN_API_KEY}`
       );
@@ -43,16 +68,20 @@ function App() {
       console.log(result);
     } catch {
       console.log("etherscan api call ended up failing");
-    }
+    } */
   }
-
+  const [isFetched, setIsFetched] = useState(false);
   const [alchemyResult, setAlchemyResult] = useState("");
   const AuthStr = "Bearer ".concat(process.env.REACT_APP_ALCHEMY_KEY);
   async function alchemyUD() {
     let result = await axios.get(
-      "https://unstoppabledomains.g.alchemy.com/domains/uns-devtest-126845.crypto",
+      `https://unstoppabledomains.g.alchemy.com/domains/${inputValue}`,
       { headers: { Authorization: AuthStr } }
     );
+    if (result) {
+      setIsFetched(true);
+    }
+    console.log(result);
     setAlchemyResult(result);
   }
   return (
@@ -76,11 +105,19 @@ function App() {
             Domain to receive personal Data.<br></br>Entering your Domain
             instead of address will give you additional data.
           </Box>
+          <Box>
+            Domain =&gt; Get NetworkID the Domain is minted on, the blockchain
+            the Domain is minted on, the owner address of the domain
+          </Box>
           <EtherscanAPI
             getAPIData={getAPIData}
             changeInput={changeInput}
             alchemyUD={alchemyUD}
             alchemyResult={alchemyResult}
+            isFetched={isFetched}
+            normalTXHistory={normalTXHistory}
+            isFetchedTX={isFetchedTX}
+            internalTXHistory={internalTXHistory}
           />
         </Box>
       </Container>
--- src/Components/EtherscanAPI.js
+++ src/Components/EtherscanAPI.js
@@ -16,18 +16,79 @@ const EtherscanAPI = (props) => {
   } */
   return (
     <div>
-      <Input onChange={(e) => props.changeInput(e)}></Input>
+      <Input
+        onChange={(e) => props.changeInput(e)}
+        placeholder="Domain or address"
+      ></Input>
       <Button onClick={(e) => props.getAPIData()}>
         Get Etherscan API Data
       </Button>
-      <Button onClick={(e) => props.alchemyUD()}>Get alchemyUD</Button>
+      {/*  <Button onClick={(e) => props.alchemyUD()}>Get alchemyUD</Button> */}
       <Box></Box>
       {/*   <Button onClick={(e) => getAPIDataInternal()}>
         Get Etherscan API ERC20 TX Events
       </Button> */}
-      <Box>{JSON.stringify(props.alchemyResult, null, "\t")}</Box>
+      {/* <Box>{JSON.stringify(props.alchemyResult, null, "\t")}</Box> */}
       <Box>List of all data i want to display</Box>
-      <Box>{props.alchemyResult.data.meta.domain}</Box>
+      {props.isFetched && (
+        <Box>
+          <Box>{props.alchemyResult.data.meta.domain}</Box>
+          <Box>{props.alchemyResult.data.meta.networkId}</Box>
+          <Box>{props.alchemyResult.data.meta.blockchain}</Box>
+          <Box>{props.alchemyResult.data.meta.owner}</Box>
+        </Box>
+      )}
+      <Box>
+        <Box>"Normal" Transaction History </Box>
+        {/*  <Box>{JSON.stringify(props.normalTXHistory, null, "\t")}</Box> */}
+        {props.isFetchedTX && (
+          <Box>
+            {/*   <Box> {JSON.stringify(props.normalTXHistory.data.result)}</Box> */}
+            {/* <Box> {props.normalTXHistory.data.result[].from}</Box> */}
+            {props.normalTXHistory.data.result.map((e, index) => {
+              return (
+                <Box>
+                  <Box>This is the {index + 1 + "th"} TX in your List</Box>
+                  <Box>
+                    From: {props.normalTXHistory.data.result[index].from}
+                  </Box>
+                  <Box>To: {props.normalTXHistory.data.result[index].to}</Box>
+                  <Box>
+                    Gas Used: {props.normalTXHistory.data.result[index].gasUsed}
+                  </Box>
+                  <Box>
+                    Gas Price:
+                    {props.normalTXHistory.data.result[index].gasPrice}
+                  </Box>
+                  <Box>
+                    Hash of TX: {props.normalTXHistory.data.result[index].hash}
+                  </Box>
+                  <Box>
+                    Value send: {props.normalTXHistory.data.result[index].value}
+                  </Box>
+                  <Box>
+                    Block-Number:
+                    {props.normalTXHistory.data.result[index].blockNumber}
+                  </Box>
+                  <Box>
+                    Contract address:
+                    {props.normalTXHistory.data.result[index].contractAddress}
+                  </Box>
+                  <Box>
+                    Input: {props.normalTXHistory.data.result[index].input}
+                  </Box>
+                </Box>
+              );
+            })}
+          </Box>
+        )}
+      </Box>
+      <Box>
+        <Box>"Internal" Transaction History </Box>
+        {props.isFetchedTX && (
+          <Box> {JSON.stringify(props.internalTXHistory.data.result)}</Box>
+        )}
+      </Box>
     </div>
   );
 };
